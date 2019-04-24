e31e8a41ec9e5ae0c385d5ce58bbbb3ff8057c64
ydl <yangdeliang@juwang.cn>
2019-04-24 18:45:29 +0800

    fix: subject min length 1

--- bash/commit-standard.sh
+++ bash/commit-standard.sh
@@ -4,7 +4,7 @@ REPLACE_CRLF_TO_LF=$(cat $1 | sed 's/~/#/g' | sed 's/[\r\n]/~/g' | tr '\n' '~')
 LEN=$(echo ${REPLACE_CRLF_TO_LF} | wc -m)
 
 test "${LEN}" = "$(cat $1 | sed 's/~/#/g' | sed 's/[\r\n]/~/g' | tr '\n' '~' |
-    grep -oP '^(feat|fix|docs|style|refactor|perf|test|chore|revert)(\([^\)]+\))?:[^~]+(~~.+(?=~~))?(~~.+)?~?' |
+    grep -oP '^(feat|fix|docs|style|refactor|perf|test|chore|revert)(\([^\)]+\))?:[^~]{1,}(~~.+(?=~~))?(~~.+)?~?' |
     wc -m)" || {
     echo >&2 [x] bad commit | cat <<\EOF
 <type>(<scope>): <subject>
