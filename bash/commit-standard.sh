59fd9610dbbe0af050e0b99f2352f75444215cd3
ydl <yangdeliang@juwang.cn>
2019-04-24 17:40:05 +0800

    feat: fix

--- bash/commit-standard.sh
+++ bash/commit-standard.sh
@@ -3,9 +3,8 @@
 REPLACE_CRLF_TO_LF=$(cat $1 | sed 's/~/#/g' | sed 's/[\r\n]/~/g' | tr '\n' '~')
 LEN=$(echo ${REPLACE_CRLF_TO_LF} | wc -m)
 
-
 test "${LEN}" = "$(cat $1 | sed 's/~/#/g' | sed 's/[\r\n]/~/g' | tr '\n' '~' |
-    grep -oP '^(feat|fix|docs|style|refactor|perf|test|chore|revert)(\([^\)]+\))?:[^~]+(~~.+(?=~~))?(~~.+)~?' |
+    grep -oP '^(feat|fix|docs|style|refactor|perf|test|chore|revert)(\([^\)]+\))?:[^~]+(~~.+(?=~~))?(~~.+)?~?' |
     wc -m)" || {
     echo >&2 [x] bad commit | cat <<\EOF
 <type>(<scope>): <subject>
